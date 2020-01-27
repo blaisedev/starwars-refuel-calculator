e1ba4b4ecb1116903483db2036db1e10742f33ac
blaisedev <blaisedevaney@gmail.com>
2020-01-27 17:08:33 +0000

    Commenting out for now

--- src/main/java/com/blaisedev/starwarsmglt/ExceptionIntercepter.java
+++ src/main/java/com/blaisedev/starwarsmglt/ExceptionIntercepter.java
@@ -1,29 +1,29 @@
-package com.blaisedev.starwarsmglt;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-
-@ControllerAdvice
-public class ExceptionIntercepter extends ResponseEntityExceptionHandler {
-
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        List<String> errorList = ex
-                .getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(fieldError -> fieldError.getDefaultMessage())
-                .collect(Collectors.toList());
-        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errorList);
-        return handleExceptionInternal(ex, apiError, headers, apiError.getStatus(), request);
-    }
-}
+//package com.blaisedev.starwarsmglt;
+//
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.context.request.WebRequest;
+//import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+//
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//
+//@ControllerAdvice
+//public class ExceptionIntercepter extends ResponseEntityExceptionHandler {
+//
+//    @Override
+//    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+//        List<String> errorList = ex
+//                .getBindingResult()
+//                .getFieldErrors()
+//                .stream()
+//                .map(fieldError -> fieldError.getDefaultMessage())
+//                .collect(Collectors.toList());
+//        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errorList);
+//        return new ResponseEntity(apiError,status);
+//    }
+//}
